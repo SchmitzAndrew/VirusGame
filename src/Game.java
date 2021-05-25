18a7f581d36c136e966c1ffdfafcb9bb803357ca
A_SCHMITZ <schmitzandrew03@gmail.com>
2021-05-25 11:47:44 -0700

    added disease and country classes

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,39 @@
+import java.util.Timer;
+
 public class Game {
+
+    double peopleKilled;
+    double worldPop;
+    int cureProgress;
+
+
+    public Game(){
+
+    }
+
+    public Country[] createCountries(){
+        Country[] countries = new Country[7];
+        countries[0] = new Country("USA", 2, 1, 30000000, 1);
+
+        return countries;
+
+
+    }
+    public boolean winCondition() {
+        if(peopleKilled - worldPop == 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean loseCondition() {
+        if (cureProgress == 100) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+
+
 public class Main {
     public static void main(String[] args){
 
@@ -9,3 +11,5 @@ public class Main {
     }
 
 }
+
+
